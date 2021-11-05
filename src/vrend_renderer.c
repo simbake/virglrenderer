2ca338a632b33749d726fec6975f3975a2df891d
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-11-05 20:25:55 +0700

    Fallback to RGBA8 (FIXME: is it correct?)
    Since we are initializing context with OSMesa which caused virgl to recognize wrong format.

--- src/vrend_renderer.c
+++ src/vrend_renderer.c
@@ -7211,9 +7211,16 @@ static int vrend_resource_alloc_texture(struct vrend_resource *gr,
       gltype = tex_conv_table[format].gltype;
 
       if (internalformat == 0) {
+         // Fallback to RGBA8 (FIXME: is it correct?) since we are initializing context with OSMesa,
+         // which caused virgl to recognize wrong format.
+         format = 2;
+         internalformat = tex_conv_table[format].internalformat;
+         glformat = tex_conv_table[format].glformat;
+         gltype = tex_conv_table[format].gltype;
+
          vrend_printf("unknown format is %d\n", pr->format);
-         glBindTexture(gr->target, 0);
-         return EINVAL;
+         //glBindTexture(gr->target, 0);
+         //return EINVAL;
       }
 
       if (pr->nr_samples > 0) {
